da94b004f8256c0f480e375a7f33758c159b70bc
JayNuan <39399274+JayNuan@users.noreply.github.com>
2018-12-02 21:43:38 +0800

    Update vim_autoinstall_by_jaynuan.sh

--- vim_autoinstall_by_jaynuan.sh
+++ vim_autoinstall_by_jaynuan.sh
@@ -11,7 +11,7 @@ for var in ${command_arr[@]}
 do
 	which $var  >> ./vim_autoinstall.log
 	rpmname_install_or_not=$(echo $?)
-	if [[ $rpmname_install_or_not -nq 0  ]]
+	if [[ $rpmname_install_or_not -ne 0  ]]
 	then
 		yum install -y $var >> ./vim_autoinstall.log
 	fi
@@ -20,10 +20,11 @@ done
 git clone https://github.com/JayNuan/vim_autoinstall
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git_sucess_or_not=$(echo $?)
-if [[ $git_sucess_or_not -nq 0 ]]
+if [[ $git_sucess_or_not -ne 0 ]]
 then
 	exit 1
 fi
+
 mkdir ~/.vim/colors
 cp -a vim_autoinstall/solarized.vim ~/.vim/colors
 cp -a vim_autoinstall/.vimrc ~/.vimrc
